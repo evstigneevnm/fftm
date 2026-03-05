e51a55681fc0da60e83c43d6c6f5b344f0b4d6b0
Cro3SwI2mer <kirill2000kartashev@gmail.com>
2026-03-06 00:22:16 +0300

    scripts/py/test-4d-fft.py: added inverse 4D-FFT

--- scripts/py/test-4d-fft.py
+++ scripts/py/test-4d-fft.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from numpy.fft import fft2, fftn
+from numpy.fft import fft2, fftn, ifft2, ifftn
 
 # Initialize random data
-n0, n1, n2, n3 = 16, 16, 16, 16
+n0, n1, n2, n3 = 32, 32, 32, 32
 X = np.random.randn(n0, n1, n2, n3) # + 1j * np.random.randn(n0, n1, n2, n3)
 
 # reference FFT
@@ -14,37 +14,37 @@ sp2 = n2 // 2
 sp3 = n3 // 2
 
 # initial split data
-X1 = X[:, :, :sp2, :sp3]
-X2 = X[:, :, sp2:, :sp3]
-X3 = X[:, :, :sp2, sp3:]
-X4 = X[:, :, sp2:, sp3:]
+FX1 = X[:, :, :sp2, :sp3]
+FX2 = X[:, :, sp2:, :sp3]
+FX3 = X[:, :, :sp2, sp3:]
+FX4 = X[:, :, sp2:, sp3:]
 
 # check if split was correct
-XS12 = np.concatenate((X1, X2), axis=2)
-XS34 = np.concatenate((X3, X4), axis=2)
+XS12 = np.concatenate((FX1, FX2), axis=2)
+XS34 = np.concatenate((FX3, FX4), axis=2)
 XS1234 = np.concatenate((XS12, XS34), axis=3)
-print(f"4D error in split: {np.linalg.norm(X - XS1234):e}")
+print(f"error in split: {np.linalg.norm(X - XS1234):e}")
 
 # first 2D-FFT
-X1h1 = fft2(X1, axes=(0, 1))
-X2h1 = fft2(X2, axes=(0, 1))
-X3h1 = fft2(X3, axes=(0, 1))
-X4h1 = fft2(X4, axes=(0, 1))
+FX1h1 = fft2(FX1, axes=(0, 1))
+FX2h1 = fft2(FX2, axes=(0, 1))
+FX3h1 = fft2(FX3, axes=(0, 1))
+FX4h1 = fft2(FX4, axes=(0, 1))
 
 # first concatenation + transpose
-X1h1_t = np.concatenate(
+FX1h1_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -52,19 +52,19 @@ X1h1_t = np.concatenate(
     axis=1
 )
 
-X2h1_t = np.concatenate(
+FX2h1_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -72,19 +72,19 @@ X2h1_t = np.concatenate(
     axis=1
 )
 
-X3h1_t = np.concatenate(
+FX3h1_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -92,19 +92,19 @@ X3h1_t = np.concatenate(
     axis=1
 )
 
-X4h1_t = np.concatenate(
+FX4h1_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -113,25 +113,25 @@ X4h1_t = np.concatenate(
 )
 
 # second 2D-FFT
-X1h2 = fft2(X1h1_t, axes=(0, 1))
-X2h2 = fft2(X2h1_t, axes=(0, 1))
-X3h2 = fft2(X3h1_t, axes=(0, 1))
-X4h2 = fft2(X4h1_t, axes=(0, 1))
+FX1h2 = fft2(FX1h1_t, axes=(0, 1))
+FX2h2 = fft2(FX2h1_t, axes=(0, 1))
+FX3h2 = fft2(FX3h1_t, axes=(0, 1))
+FX4h2 = fft2(FX4h1_t, axes=(0, 1))
 
 # second concatenation + transpose (reverse)
-X1h2_t = np.concatenate(
+FX1h2_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -139,19 +139,19 @@ X1h2_t = np.concatenate(
     axis=1
 )
 
-X2h2_t = np.concatenate(
+FX2h2_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -159,19 +159,19 @@ X2h2_t = np.concatenate(
     axis=1
 )
 
-X3h2_t = np.concatenate(
+FX3h2_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -179,19 +179,19 @@ X3h2_t = np.concatenate(
     axis=1
 )
 
-X4h2_t = np.concatenate(
+FX4h2_t = np.concatenate(
     (
         np.concatenate(
             (
-                np.transpose(X1h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X2h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX1h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX2h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         ), 
         np.concatenate(
             (
-                np.transpose(X3h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
-                np.transpose(X4h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+                np.transpose(FX3h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(FX4h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
             ), 
             axis=0
         )
@@ -200,7 +200,204 @@ X4h2_t = np.concatenate(
 )
 
 # final concatenation
-XS12 = np.concatenate((X1h2_t, X2h2_t), axis=2)
-XS34 = np.concatenate((X3h2_t, X4h2_t), axis=2)
-XS1234 = np.concatenate((XS12, XS34), axis=3)
-print(f"4D error in split: {np.linalg.norm(X_hat - XS1234):e}")
+FXS12 = np.concatenate((FX1h2_t, FX2h2_t), axis=2)
+FXS34 = np.concatenate((FX3h2_t, FX4h2_t), axis=2)
+FXS1234 = np.concatenate((FXS12, FXS34), axis=3)
+print(f"error in fft: {np.linalg.norm(X_hat - FXS1234):e}")
+
+# inverse 4D-FFT
+X_ref = ifftn(X_hat)
+
+# inverse 4D-FFT with slab-slab decomposition begins here
+
+# initial split data
+BX1 = FXS1234[:, :, :sp2, :sp3]
+BX2 = FXS1234[:, :, sp2:, :sp3]
+BX3 = FXS1234[:, :, :sp2, sp3:]
+BX4 = FXS1234[:, :, sp2:, sp3:]
+
+# check if split was correct
+BXS12 = np.concatenate((BX1, BX2), axis=2)
+BXS34 = np.concatenate((BX3, BX4), axis=2)
+BXS1234 = np.concatenate((BXS12, BXS34), axis=3)
+print(f"error in inverse split: {np.linalg.norm(FXS1234 - BXS1234):e}")
+
+# first inverse 2D-FFT
+BX1h1 = ifft2(BX1, axes=(0, 1))
+BX2h1 = ifft2(BX2, axes=(0, 1))
+BX3h1 = ifft2(BX3, axes=(0, 1))
+BX4h1 = ifft2(BX4, axes=(0, 1))
+
+# first concatenation + transpose
+BX1h1_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h1[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX2h1_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h1[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX3h1_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h1[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX4h1_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h1[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+# second inverse 2D-FFT
+BX1h2 = ifft2(BX1h1_t, axes=(0, 1))
+BX2h2 = ifft2(BX2h1_t, axes=(0, 1))
+BX3h2 = ifft2(BX3h1_t, axes=(0, 1))
+BX4h2 = ifft2(BX4h1_t, axes=(0, 1))
+
+# second concatenation + transpose (reverse)
+BX1h2_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h2[:sp2, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX2h2_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h2[sp2:, :sp3, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX3h2_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h2[:sp2, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+BX4h2_t = np.concatenate(
+    (
+        np.concatenate(
+            (
+                np.transpose(BX1h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX2h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        ), 
+        np.concatenate(
+            (
+                np.transpose(BX3h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1)), 
+                np.transpose(BX4h2[sp2:, sp3:, :, :], axes=(2, 3, 0, 1))
+            ), 
+            axis=0
+        )
+    ), 
+    axis=1
+)
+
+# final concatenation
+BXS12 = np.concatenate((BX1h2_t, BX2h2_t), axis=2)
+BXS34 = np.concatenate((BX3h2_t, BX4h2_t), axis=2)
+BXS1234 = np.concatenate((BXS12, BXS34), axis=3)
+print(f"error in inverse 4D-fft: {np.linalg.norm(X_ref - BXS1234):e}")
